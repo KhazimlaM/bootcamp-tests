db1344d10de1d6aceb823101ee769e14050bf038
Khazimla <20113497@rcconnect.co.za>
2022-04-26 16:44:41 +0200

    last changes

--- test/countAllFromTown.tests.js
+++ test/countAllFromTown.tests.js
@@ -7,8 +7,8 @@ describe('Test for my countAllFromtown function' , function(){
         assert.equal('1',countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'));
         
     });
-    it("it should return '1' if the registration numbers passed in the function are 'CJ 124,CY 567,CL 345, CF 456, CL 341','CF' ", function(){
-        assert.equal('1',countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'));
+    it("it should return '0' if the registration numbers passed in the function are '' ", function(){
+        assert.equal('0',countAllFromTown(''));
         
     });
 

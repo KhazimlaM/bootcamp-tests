fa4fe852c133275ba791fc3bda0eb96aedf54e30
Khazimla <20113497@rcconnect.co.za>
2022-04-26 16:25:40 +0200

    my tests

--- test/isWeekday.tests.js
+++ test/isWeekday.tests.js
@@ -1,14 +1,14 @@
 describe('Test for my isWeekday function' , function(){
-    it("it should return 'false' if it starts wiith 'S' " , function(){
-        assert.equal(false,isWeekday("S"));
+    it("it should return 'false' if it called with a word that starts with 'S' " , function(){
+        assert.equal(false,isWeekday("Sunday"));
 
     });
     it("it should return 'true' if it starts with 'M || T  || W || T || F' " , function(){
-        assert.equal(true,isWeekday("W"));
+        assert.equal(true,isWeekday("Thursday"));
 
     });
     it("it should return 'true' if it starts with 'M || T  || W || T || F' " , function(){
-        assert.equal(true,isWeekday("F"));
+        assert.equal(true,isWeekday("Friday"));
 
     });
 
